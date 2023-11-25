3114d709b1d566154d7363abe743091987ad58c4
Vinícius Yoda <vyoda4604@gmail.com>
2023-11-25 13:59:32 -0300

    test

---
+++ src/paginas/Principal/src/paginas/Principal/App.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import AppRoutes from '../../routes';
+
+describe('Componente <App/>', () => {
+  test('Deve permitir adicionar uma transação em Extrato', () => {
+    render(<App />, { wrapper: BrowserRouter });
+
+    const select = screen.getByRole('combobox');
+    const campoValor = screen.getByPlaceholderText('Digite um valor');
+    const botao = screen.getByRole('button');
+
+    userEvent.selectOptions(select, ['Depósito']);
+    userEvent.type(campoValor, '100');
+    userEvent.click(botao);
+
+    const novaTransacao = screen.getByTestId('lista-transacoes');
+    const itemExtrato = screen.getByRole('listitem');
+
+    expect(novaTransacao).toContainElement(itemExtrato);
+  });
+
+  test('Deve navegar até a página correspondente ao link clicado', async () => {
+    render(<AppRoutes />, { wrapper: BrowserRouter });
+
+    const linkPaginaCartoes = screen.getByText('Cartões');
+    expect(linkPaginaCartoes).toBeInTheDocument();
+
+    userEvent.click(linkPaginaCartoes);
+
+    const tituloPaginaCartoes = await screen.findByText('Meus cartões');
+    expect(tituloPaginaCartoes).toBeInTheDocument();
+  });
+});
